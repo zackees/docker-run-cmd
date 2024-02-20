221a4ac5a1514fb46211041f4ada9ea403747fb4
zackees <z@zackees.com>
2024-02-19 23:55:45 -0800

    update

--- tests/test_run2.py
+++ tests/test_run2.py
@@ -17,16 +17,22 @@ assert DOCKER_FILE.exists(), f"Dockerfile not found: {DOCKER_FILE}"
 class MainTester(unittest.TestCase):
     """Main tester class."""
 
+    @unittest.skip("Skip this test")
     def test_imports(self) -> None:
         """Test command line interface (CLI)."""
         run(name="yt-dlp-docker", dockerfile_or_url=DOCKER_FILE, cwd=PROJECT_DIR / "tmp", cmd_list=["--help"])
         print()
 
     # video https://www.youtube.com/watch?v=mh_hNvGTmIg
+    @unittest.skip("Skip this test")
     def test_url(self) -> None:
         url = "https://www.youtube.com/watch?v=mh_hNvGTmIg"
         run(name="yt-dlp-docker", dockerfile_or_url=DOCKER_FILE, cwd=PROJECT_DIR / "tmp", cmd_list=[url])
 
+    # https://www.brighteon.com/f77d70c8-f5a9-44fa-8125-dc3a6ac85643
+    def test_url2(self) -> None:
+        url = "https://www.brighteon.com/f77d70c8-f5a9-44fa-8125-dc3a6ac85643"
+        run(name="yt-dlp-docker", dockerfile_or_url=DOCKER_FILE, cwd=PROJECT_DIR / "tmp", cmd_list=[url])
 
 
 if __name__ == "__main__":
